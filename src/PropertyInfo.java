9648c9816034e29fbbf29afe0479b50ec0b46c43
wredwards94 <115098566+wredwards94@users.noreply.github.com>
2023-03-01 05:14:41 +0000

    Added 'available properties' lines

--- src/PropertyInfo.java
+++ src/PropertyInfo.java
@@ -6,6 +6,7 @@ public class PropertyInfo {
         do {
             System.out.println("");
             System.out.println("Please enter a valid property for more information or 'back' for main menu:");
+          System.out.println("Available properties: [EVEN, ODD, BUZZ, DUCK, PALINDROMIC, GAPFUL, SPY, SQUARE, SUNNY]");
             input = scanner.nextLine();
         }while (!input.equalsIgnoreCase("back"));
     }
