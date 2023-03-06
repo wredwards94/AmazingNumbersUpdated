0dcbc4013b483ecc39007130934674cd109d1a98
wesleyedwards <wredwards94@outlook.com>
2023-03-06 07:58:33 -0500

    refactored num.length to twoProps[0].length in 2nums2props

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         System.out.println("Welcome to Amazing Numbers!");
         GetMenu.menu();
     }
